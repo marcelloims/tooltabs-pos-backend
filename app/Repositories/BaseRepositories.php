c569319ac68a27572269a9ba208ec236aee16a62
Marcello Imanuel <marcelloimanuel25@gmail.com>
2024-08-29 22:46:32 +0800

    - install jwt - config provider - generate jwt secret

---
+++ app/Repositories/BaseRepositories.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+class BaseRepositories {
+
+    public function getAll(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function destroy(){
+
+    }
+}
